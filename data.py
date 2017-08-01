69b6ca3eb49b329fe49d3be7f133fc65c060c500
michalkoziarski <michalkoziarski@gmail.com>
2017-08-01 17:41:52 +0200

    model & data manipulation

---
+++ data.py
@@ -0,0 +1,219 @@
+import os
+import zipfile
+import numpy as np
+
+from scipy import misc
+from urllib.request import urlretrieve
+
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+
+
+class TrainSet:
+    def __init__(self, benchmark, batch_size):
+        self.benchmark = benchmark
+        self.batch_size = batch_size
+        self.images_completed = 0
+        self.epochs_completed = 0
+        self.root_path = os.path.join(DATA_PATH, 'train', '%s_augmented' % self.benchmark)
+        self.paths = os.listdir(self.root_path)
+        self.images = []
+        self.targets = []
+
+        for file_name in self.paths:
+            full_name, extension = file_name.split('.')
+            original_name, count, scaling_factor = full_name.split('-')
+
+            if scaling_factor != '1':
+                target_name = '%s-%s-1.%s' % (original_name, count, extension)
+
+                self.images.append(self.__read_image(file_name))
+                self.targets.append(self.__read_image(target_name))
+
+        self.images = np.array(self.images)
+        self.targets = np.array(self.targets)
+
+        self.shuffle()
+        self.length = len(self.images)
+        self.length = self.length - self.length % self.batch_size
+        self.images = self.images[:self.length]
+        self.targets = self.targets[:self.length]
+
+    def batch(self):
+        images = self.images[self.images_completed:(self.images_completed + self.batch_size)]
+        targets = self.targets[self.images_completed:(self.images_completed + self.batch_size)]
+
+        self.images_completed += self.batch_size
+
+        if self.images_completed >= self.length:
+            self.images_completed = 0
+            self.epochs_completed += 1
+            self.shuffle()
+
+        return images, targets
+
+    def shuffle(self):
+        indices = list(range(len(self.images)))
+        np.random.shuffle(indices)
+        self.images = self.images[indices]
+        self.targets = self.targets[indices]
+
+    def __read_image(self, file_name):
+        return np.expand_dims((misc.imread(os.path.join(self.root_path, file_name)).astype(np.float) / 255), axis=2)
+
+
+class TestSet:
+    def __init__(self, benchmark):
+        self.benchmark = benchmark
+        self.images_completed = 0
+        self.root_path = os.path.join(DATA_PATH, 'test', '%s_augmented' % self.benchmark)
+        self.paths = os.listdir(self.root_path)
+        self.images = []
+        self.targets = []
+
+        for file_name in self.paths:
+            full_name, extension = file_name.split('.')
+            original_name, count, scaling_factor = full_name.split('-')
+
+            if scaling_factor != '1':
+                target_name = '%s-%s-1.%s' % (original_name, count, extension)
+
+                self.images.append(self.__read_image(file_name))
+                self.targets.append(self.__read_image(target_name))
+
+        self.length = len(self.images)
+
+    def fetch(self):
+        if self.images_completed >= self.length:
+            return None
+        else:
+            self.images_completed += 1
+
+            return np.array([self.images[self.images_completed - 1]]), np.array([self.targets[self.images_completed - 1]])
+
+    def __read_image(self, file_name):
+        return np.expand_dims((misc.imread(os.path.join(self.root_path, file_name)).astype(np.float) / 255), axis=2)
+
+
+def download():
+    if not os.path.exists(DATA_PATH):
+        os.mkdir(DATA_PATH)
+
+    for partition in ['train', 'test']:
+        partition_path = os.path.join(DATA_PATH, partition)
+        zip_path = os.path.join(partition_path, '%s_data.zip' % partition)
+        url = 'http://cv.snu.ac.kr/research/VDSR/%s_data.zip' % partition
+
+        if not os.path.exists(partition_path):
+            os.mkdir(partition_path)
+
+        if not os.path.exists(zip_path):
+            print('Downloading %s data...' % partition)
+
+            urlretrieve(url, zip_path)
+
+        with zipfile.ZipFile(zip_path) as f:
+            print('Extracting %s data...' % partition)
+
+            f.extractall(partition_path)
+
+
+def augment(partition, benchmark, patch_size=41):
+    benchmark_path = os.path.join(DATA_PATH, partition, benchmark)
+    augmented_path = os.path.join(DATA_PATH, partition, '%s_augmented' % benchmark)
+
+    if not os.path.exists(augmented_path):
+        os.mkdir(augmented_path)
+
+    if partition == 'train':
+        for file_name in os.listdir(benchmark_path):
+            image = misc.imread(os.path.join(benchmark_path, file_name), mode='YCbCr')[:, :, 0]
+            width, height = image.shape
+            width = width - width % 12
+            height = height - height % 12
+            n_horizontal_patches = width // patch_size
+            n_vertical_patches = height // patch_size
+            image = image[:width, :height]
+            scaled_images = {1.0: image}
+
+            for scale in [2.0, 3.0, 4.0]:
+                downscaled = misc.imresize(image, 1.0 / scale, 'bicubic')
+                scaled_images[scale] = misc.imresize(downscaled, scale, 'bicubic')
+
+            for scale in [1.0, 2.0, 3.0, 4.0]:
+                count = 0
+
+                for horizontal_patch in range(n_horizontal_patches):
+                    for vertical_patch in range(n_vertical_patches):
+                        h_start = horizontal_patch * patch_size
+                        v_start = vertical_patch * patch_size
+                        patch = scaled_images[scale][h_start:h_start + patch_size, v_start:v_start + patch_size]
+
+                        for flip in [True, False]:
+                            if flip:
+                                flipped = np.fliplr(patch)
+                            else:
+                                flipped = patch
+
+                            for angle in [0, 90, 180, 270]:
+                                rotated = misc.imrotate(flipped, angle, 'bicubic')
+
+                                name, extension = file_name.split('.')
+                                count += 1
+                                out_name = '%s-%d-%d.%s' % (name, count, scale, extension)
+                                misc.imsave(os.path.join(augmented_path, out_name), rotated)
+
+    if partition == 'test':
+        for file_name in os.listdir(benchmark_path):
+            image = misc.imread(os.path.join(benchmark_path, file_name), mode='YCbCr')[:, :, 0]
+            width, height = image.shape
+            width = width - width % 12
+            height = height - height % 12
+            image = image[:width, :height]
+            scaled_images = {1.0: image}
+
+            for scale in [2.0, 3.0, 4.0]:
+                downscaled = misc.imresize(image, 1.0 / scale, 'bicubic')
+                scaled_images[scale] = misc.imresize(downscaled, scale, 'bicubic')
+
+            for scale in [1.0, 2.0, 3.0, 4.0]:
+                name, extension = file_name.split('.')
+                out_name = '%s-1-%d.%s' % (name, scale, extension)
+                misc.imsave(os.path.join(augmented_path, out_name), scaled_images[scale])
+
+
+def load(partition, benchmark, batch_size=64, patch_size=41):
+    assert partition in ['train', 'test']
+
+    if partition == 'train':
+        assert benchmark in ['91', '291']
+    else:
+        assert benchmark in ['B100', 'Set5', 'Set14', 'Urban100']
+
+    benchmark_path = os.path.join(DATA_PATH, partition, benchmark)
+    augmented_path = os.path.join(DATA_PATH, partition, '%s_augmented' % benchmark)
+
+    if not os.path.exists(benchmark_path):
+        print('Downloading data...')
+
+        download()
+
+        print('Downloading complete.')
+
+    if not os.path.exists(augmented_path):
+        print('Augmenting data...')
+
+        augment(partition, benchmark, patch_size)
+
+        print('Augmenting complete.')
+
+    print('Loading data to the memory...')
+
+    if partition == 'train':
+        data_set = TrainSet(benchmark, batch_size)
+    else:
+        data_set = TestSet(benchmark)
+
+    print('Loading complete.')
+
+    return data_set
---
+++ model.py
@@ -0,0 +1,35 @@
+import numpy as np
+import tensorflow as tf
+
+
+class Model:
+    def __init__(self, input, n_layers=20, k=3, n_filters=64):
+        self.input = input
+        self.n_layers = n_layers
+        self.k = k
+        self.n_filters = n_filters
+        self.weights = []
+        self.biases = []
+        self.output = self.input
+        
+        for i in range(self.n_layers):
+            if i == 0:
+                in_shape = 1
+            else:
+                in_shape = self.n_filters
+
+            if i == self.n_layers - 1:
+                out_shape = 1
+            else:
+                out_shape = self.n_filters
+
+            weight = tf.Variable(tf.random_normal([self.k, self.k, in_shape, out_shape],
+                                                  stddev=np.sqrt(2 / (k ** 2 * in_shape))))
+            bias = tf.Variable(tf.zeros([out_shape]))
+
+            self.weights.append(weight)
+            self.biases.append(bias)
+            self.output = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(self.output, weight,
+                                                                 strides=[1, 1, 1, 1], padding='SAME'), bias))
+
+        self.output = tf.add(self.output, self.input)
