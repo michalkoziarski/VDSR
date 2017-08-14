4716ba4263cf3283c61f0450344a042feb3c7321
michalkoziarski <michalkoziarski@gmail.com>
2017-08-15 00:28:38 +0200

    simpler PSNR formula

--- utils.py
+++ utils.py
@@ -3,4 +3,4 @@ import tensorflow as tf
 
 
 def psnr(x, y, maximum=1.0):
-    return 20 * np.log10(maximum) - 10 * tf.log(tf.maximum(tf.reduce_mean(tf.pow(x - y, 2)), 1e-20)) / np.log(10)
+    return 20 * tf.log(maximum / tf.sqrt(tf.reduce_mean(tf.pow(x - y, 2)))) / np.log(10)
